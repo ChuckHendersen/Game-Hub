a999d00dfc7f65521053be134231e25022813927
Giovanni Grieco <giovi.5057@gmail.com>
2023-03-03 20:10:04 +0100

    aggiunto commento nel main

--- src/main/java/com/example/demo/ProgettoSiwApplication.java
+++ src/main/java/com/example/demo/ProgettoSiwApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProgettoSiwApplication {
 
 	public static void main(String[] args) {
+		//Commento per testare git
 		SpringApplication.run(ProgettoSiwApplication.class, args);
 	}
 
