3502ee7ff4bd25d0cac2a0b1768c0216617a5913
Giovanni Grieco <giovi.5057@gmail.com>
2023-03-14 14:41:17 +0100

    Caricata su maven la dipendenza alla libreria di Steam API

--- src/main/java/it/uniroma3/siw/GameHub/ProgettoSiwApplication.java
+++ src/main/java/it/uniroma3/siw/GameHub/ProgettoSiwApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package it.uniroma3.siw.GameHub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
