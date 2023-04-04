11a0e63945576e59feb6fc418cb1b21f258e4ef6
giovi <giovi@miolaptop>
2023-04-04 14:15:44 +0200

    niente

--- src/main/java/it/uniroma3/siw/GameHub/SteamAPI.java
+++ src/main/java/it/uniroma3/siw/GameHub/SteamAPI.java
@@ -4,5 +4,7 @@ import com.lukaspradel.steamapi.webapi.client.SteamWebApiClient;
 
 // Esiste solo un'istanza di SteamWebApiClient
 public class SteamAPI {
+	
 	public static SteamWebApiClient client = new SteamWebApiClient.SteamWebApiClientBuilder("056BDA5087E6B09FF4E875FD1ACAFD3F").build();
+
 }
