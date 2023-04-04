e3e35e534448b4682c13774b07dd53e8e23f4426
giovi <giovi@miolaptop>
2023-04-04 14:44:41 +0200

    Continuo su windows

--- src/main/java/it/uniroma3/siw/GameHub/controller/WebUserController.java
+++ src/main/java/it/uniroma3/siw/GameHub/controller/WebUserController.java
@@ -2,8 +2,12 @@ package it.uniroma3.siw.GameHub.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import it.uniroma3.siw.GameHub.model.WebUser;
 import it.uniroma3.siw.GameHub.repository.WebUserRepository;
 
 @Controller
@@ -12,7 +16,18 @@ public class WebUserController {
 	WebUserRepository webUserRepository;
 	
 	@GetMapping("/formNewWebUser")
-	public String formNewWebUser() {
+	public String formNewWebUser(Model model) {
+		model.addAttribute("webUser", new WebUser());
 		return "formNewWebUser.html";
+	}
+	
+	@PostMapping("/webUsers")
+	public String newWebUser(@ModelAttribute("webUser") WebUser wu, Model model){
+		if(!webUserRepository.existsByUserEmail(wu.getUserEmail())) {
+			model.add
+		}else {
+			
+		}
+		return "webUser.html";
 	}
 }
--- src/main/java/it/uniroma3/siw/GameHub/repository/WebUserRepository.java
+++ src/main/java/it/uniroma3/siw/GameHub/repository/WebUserRepository.java
@@ -1,13 +1,12 @@
 package it.uniroma3.siw.GameHub.repository;
-
-import java.util.List;
+import java.util.Set;
 
 import org.springframework.data.repository.CrudRepository;
 import it.uniroma3.siw.GameHub.model.WebUser;
 
 public interface WebUserRepository extends CrudRepository<WebUser, Long> {
-	public List<WebUser> findByUsername(String username);
+	public Set<WebUser> findByUsername(String username);
 	
 	public boolean existsByUsername(String username);
-	public boolean existsByUserEmail(String username);
+	public boolean existsByUserEmail(String email);
 }
