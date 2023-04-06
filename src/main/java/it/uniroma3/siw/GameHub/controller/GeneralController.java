00a85ac8f076f63a622db57e2c666cffd89083d9
giovi <giovi@miolaptop>
2023-04-06 09:37:01 +0200

    TBD controllare precedente commit

--- src/main/java/it/uniroma3/siw/GameHub/controller/GeneralController.java
+++ src/main/java/it/uniroma3/siw/GameHub/controller/GeneralController.java
@@ -3,19 +3,11 @@ package it.uniroma3.siw.GameHub.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.lukaspradel.steamapi.core.exception.SteamApiException;
-import com.lukaspradel.steamapi.data.json.friendslist.Friend;
-import com.lukaspradel.steamapi.data.json.friendslist.GetFriendList;
-import com.lukaspradel.steamapi.webapi.request.GetFriendListRequest;
-import com.lukaspradel.steamapi.webapi.request.builders.SteamWebApiRequestFactory;
-
-import it.uniroma3.siw.GameHub.SteamAPI;
-
 @Controller
 public class GeneralController {
 	
 	@GetMapping("/")
-	public String index() throws SteamApiException {
+	public String index(){
 		return "index.html";
 	}
 }
