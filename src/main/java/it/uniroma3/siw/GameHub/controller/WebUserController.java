5d6dc0f0cf7d1855b1bdae4cf63c0cbf52cf8c95
Giovanni Grieco <giovi.5057@gmail.com>
2023-04-06 13:32:02 +0200

    boh?

--- src/main/java/it/uniroma3/siw/GameHub/controller/WebUserController.java
+++ src/main/java/it/uniroma3/siw/GameHub/controller/WebUserController.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import it.uniroma3.siw.GameHub.model.WebUser;
 import it.uniroma3.siw.GameHub.repository.WebUserRepository;
 
@@ -21,12 +21,30 @@ public class WebUserController {
 		return "formNewWebUser.html";
 	}
 	
+	@GetMapping("/webUsers")
+	public String webUsers(Model model) {
+		return "webUsers.html";
+	}
+	
 	@PostMapping("/webUsers")
 	public String newWebUser(@ModelAttribute("webUser") WebUser wu, Model model){
 		if(!webUserRepository.existsByUserEmail(wu.getUserEmail())) {
-			model.add
+			model.addAttribute("webUser", wu);
+			webUserRepository.save(wu);
+			return "webUser.html";
 		}else {
-			
+			model.addAttribute("messaggioErrore", "Utente già esistente");
+			return "formNewWebUser.html";
+		}
+	}
+	
+	@GetMapping("/webUser/{webUser_id}")
+	public String webUser(@PathVariable("webUser_id") Long id, Model model) {
+		try {
+			WebUser wu = webUserRepository.findById(id).get();
+			model.addAttribute("webUser", wu);
+		}catch(Exception e) {
+			model.addAttribute("messaggioErrore", "L'utente non esiste");
 		}
 		return "webUser.html";
 	}
