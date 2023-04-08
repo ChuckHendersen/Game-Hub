3c5fac8dde5cdecf4a44b698c94ef4f70f3bcbef
giovi <giovi@miolaptop>
2023-04-08 19:03:39 +0200

    Finire pagina html che mostri i giochi

--- src/main/java/it/uniroma3/siw/GameHub/controller/GameController.java
+++ src/main/java/it/uniroma3/siw/GameHub/controller/GameController.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 import com.lukaspradel.steamapi.core.exception.SteamApiException;
 import com.lukaspradel.steamapi.data.json.ownedgames.GetOwnedGames;
@@ -22,27 +23,8 @@ public class GameController {
 	@Autowired
 	public GameRepository gameRepository;
 	
-	static public void RefreshGames(WebUser wu, WebUserRepository wur, GameRepository gr) throws SteamApiException {
-		GetOwnedGamesRequest request =  SteamWebApiRequestFactory.createGetOwnedGamesRequest(wu.getSteamID64());
-		GetOwnedGames gog = SteamAPI.client.<GetOwnedGames>processRequest(request);
-		System.out.println("Giochi posseduti: "+gog.getResponse().getGames().size());
-		Set<Game> insiemeGiochi = new HashSet<>();
-		for(com.lukaspradel.steamapi.data.json.ownedgames.Game apiGame : gog.getResponse().getGames() ) {
-			Game g = new Game();
-//			System.out.println(apiGame.getName());
-//			System.out.println(apiGame.getAppid());
-			g.setSteamcode(apiGame.getAppid());
-			g.setGamename(apiGame.getName());
-			
-			if(!gr.existsBySteamcode(g.getSteamcode())) {
-				gr.save(g);
-			}
-			
-			insiemeGiochi.add(g);
-		}
-		wu.setOwnedGames(insiemeGiochi);
-		System.out.println(insiemeGiochi);
-		wur.save(wu);
+	public String games(Model model) {
+		return "games.html";
 	}
 	
 }
--- src/main/java/it/uniroma3/siw/GameHub/controller/WebUserController.java
+++ src/main/java/it/uniroma3/siw/GameHub/controller/WebUserController.java
@@ -1,6 +1,8 @@
 package it.uniroma3.siw.GameHub.controller;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.lukaspradel.steamapi.core.exception.SteamApiException;
+import com.lukaspradel.steamapi.data.json.ownedgames.GetOwnedGames;
+import com.lukaspradel.steamapi.webapi.request.GetOwnedGamesRequest;
+import com.lukaspradel.steamapi.webapi.request.builders.SteamWebApiRequestFactory;
+
+import it.uniroma3.siw.GameHub.SteamAPI;
+import it.uniroma3.siw.GameHub.model.Game;
 import it.uniroma3.siw.GameHub.model.WebUser;
 import it.uniroma3.siw.GameHub.repository.GameRepository;
 import it.uniroma3.siw.GameHub.repository.WebUserRepository;
@@ -65,16 +75,39 @@ public class WebUserController {
 	}
 	
 	@GetMapping("/updateOwnedGames/{id}")
-	public String updateOwnedGames(@PathVariable("id") Long id, Model model) {
-		WebUser wu = null;
-		try {
-			wu=webUserRepository.findById(id).get();
-			GameController.RefreshGames(wu,webUserRepository,gameRepository);
-			model.addAttribute("webUser", wu);
-		}catch(Exception e) {
-			e.printStackTrace();
-			model.addAttribute("messaggioErrore", "Utente non esistente");
+	public String RefreshGames(Model model, @PathVariable("id") Long id) throws SteamApiException {
+		WebUser wu = this.getWebUserById(id);
+		if(wu==null) {
+			model.addAttribute("messaggioErrore", "utente non trovato");
+			return "webUser.html";
 		}
+		GetOwnedGamesRequest request =  SteamWebApiRequestFactory.createGetOwnedGamesRequest(wu.getSteamID64());
+		GetOwnedGames gog = SteamAPI.client.<GetOwnedGames>processRequest(request);
+		System.out.println("Giochi posseduti: "+gog.getResponse().getGames().size());
+		Set<Game> insiemeGiochi = wu.getOwnedGames();
+		for(com.lukaspradel.steamapi.data.json.ownedgames.Game apiGame : gog.getResponse().getGames() ) {
+			Game g = new Game();
+//			System.out.println(apiGame.getName());
+//			System.out.println(apiGame.getAppid());
+			g.setSteamcode(apiGame.getAppid());
+			g.setGamename(apiGame.getName());
+			
+			if(!gameRepository.existsBySteamcode(g.getSteamcode())) {
+				gameRepository.save(g);
+				insiemeGiochi.add(g);
+			}
+		}
+		System.out.println(insiemeGiochi);
+		webUserRepository.save(wu);
+		model.addAttribute("webUser", wu);
 		return "webUser.html";
+	}
+	
+	public WebUser getWebUserById(Long id) {
+		try {
+			return webUserRepository.findById(id).get();
+		}catch (Exception e){
+			return null;
+		}
 	}
 }
