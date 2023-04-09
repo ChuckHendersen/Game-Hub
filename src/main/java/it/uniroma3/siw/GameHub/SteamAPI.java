1d7df03111ea4298ebf97487cd91b20bc84e3395
Giovanni Grieco <giovi.5057@gmail.com>
2023-04-09 12:40:30 +0200

    aggiunto commento per quanto riguarda la sicurezza

--- src/main/java/it/uniroma3/siw/GameHub/SteamAPI.java
+++ src/main/java/it/uniroma3/siw/GameHub/SteamAPI.java
@@ -4,5 +4,6 @@ import com.lukaspradel.steamapi.webapi.client.SteamWebApiClient;
 
 // Esiste solo un'istanza di SteamWebApiClient
 public class SteamAPI {
+	// E' lecito lasciare la chiave in chiaro in questo modo??
 	public static SteamWebApiClient client = new SteamWebApiClient.SteamWebApiClientBuilder("056BDA5087E6B09FF4E875FD1ACAFD3F").build();
 }
