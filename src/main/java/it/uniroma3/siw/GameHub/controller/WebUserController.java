55e6dfded5357341176e2c34b99cd38992f35d30
Lorenzo Battisti <114929610+LorenzoBattistiRomaTre@users.noreply.github.com>
2023-04-27 10:55:57 +0200

    ora funziona sul serio

--- src/main/java/it/uniroma3/siw/GameHub/controller/WebUserController.java
+++ src/main/java/it/uniroma3/siw/GameHub/controller/WebUserController.java
@@ -1,4 +1,5 @@
 package it.uniroma3.siw.GameHub.controller;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -12,7 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.lukaspradel.steamapi.core.exception.SteamApiException;
 import com.lukaspradel.steamapi.data.json.ownedgames.GetOwnedGames;
+import com.lukaspradel.steamapi.data.json.recentlyplayedgames.GetRecentlyPlayedGames;
 import com.lukaspradel.steamapi.webapi.request.GetOwnedGamesRequest;
+import com.lukaspradel.steamapi.webapi.request.GetRecentlyPlayedGamesRequest;
+import com.lukaspradel.steamapi.webapi.request.builders.SteamWebApiRequestFactory;
 
 import it.uniroma3.siw.GameHub.SteamAPI;
 import it.uniroma3.siw.GameHub.model.Game;
@@ -24,13 +28,13 @@ import it.uniroma3.siw.GameHub.repository.WebUserRepository;
 public class WebUserController {
 	@Autowired 
 	WebUserRepository webUserRepository;
-	
+
 	@Autowired 
 	GameRepository gameRepository;
-	
+
 	@Autowired
 	SteamAPI steamApi;
-	
+
 	@GetMapping("/webUsers")
 	public String webUsers(Model model) {
 		List<WebUser> lista =(List<WebUser>) webUserRepository.findAll();
@@ -40,36 +44,43 @@ public class WebUserController {
 		}
 		return "webUsers.html";
 	}
-	
+
 	@GetMapping("/webUser/{id}")
-	public String webUser(@PathVariable("id") Long id,Model model) {
+	public String webUser(@PathVariable("id") Long id,Model model) throws SteamApiException {
 		WebUser wu=getWebUserById(id);
 		if(wu==null) {
 			model.addAttribute("messaggioErrore", "Utente non trovato");
-			return "webUser.html";
+			return "webUser.html"; 
 		}
 		model.addAttribute("webUser", wu);
+		List <Game> lista = top5Games(wu);
+		System.out.println(lista);
+		System.out.println(lista.size());
+		for(Game g: lista) {
+			System.out.println(g);
+		}
+		model.addAttribute("top5Played", lista);
 		return "webUser.html";
 	}
-	
+
 	@GetMapping("/formNewWebUser")
 	public String formNewWebUser(Model model) {
 		model.addAttribute("webUser", new WebUser());
 		return "formNewWebUser.html";
 	}
-	
+
 	@PostMapping("/webUsers")
-	public String newWebUser(@ModelAttribute("webUser") WebUser wu, Model model){
+	public String newWebUser(@ModelAttribute("webUser") WebUser wu, Model model) throws SteamApiException{
 		if(!webUserRepository.existsByEmail(wu.getEmail())) {
 			model.addAttribute("webUser", wu);
 			webUserRepository.save(wu);
-			return "webUser.html"; 
+			return "webUser.html";
 		}else {
 			model.addAttribute("messaggioErrore", "Utente già esistente");
 			return "formNewWebUser.html";
 		}
 	}
-	
+
 	@GetMapping("/updateOwnedGames/{id}")
 	public String RefreshGames(Model model, @PathVariable("id") Long id) throws SteamApiException {
 		WebUser wu = this.getWebUserById(id);
@@ -93,7 +104,7 @@ public class WebUserController {
 		webUserRepository.save(wu);
 		return "redirect:/webUser/"+wu.getId().toString();
 	}
-	
+
 	private WebUser getWebUserById(Long id) {
 		try {
 			return webUserRepository.findById(id).get();
@@ -101,5 +112,19 @@ public class WebUserController {
 			e.printStackTrace();
 			return null;
 		}
+	}
+
+	private List<Game> top5Games(WebUser user) throws SteamApiException{
+		List<Game> topList= new ArrayList<Game>();
+		GetRecentlyPlayedGamesRequest request= SteamWebApiRequestFactory.createGetRecentlyPlayedGamesRequest(user.getSteamId(), 5);
+		GetRecentlyPlayedGames answer= steamApi.getClient().<GetRecentlyPlayedGames>processRequest(request);
+		for(com.lukaspradel.steamapi.data.json.recentlyplayedgames.Game apiGame : answer.getResponse().getGames() ) {
+			Game g = new Game();
+			g.setSteamcode(apiGame.getAppid());
+			g.setName(apiGame.getName());/**/
+			gameRepository.save(g);
+			topList.add(g);
+		}
+		return topList;
 	}
 }
