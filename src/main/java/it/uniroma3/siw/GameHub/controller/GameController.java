130fec80436ebbc0f89bd48ce8db9c3f728f5715
Lorenzo Battisti <114929610+LorenzoBattistiRomaTre@users.noreply.github.com>
2023-06-09 15:18:18 +0200

    PORCODDIO

--- src/main/java/it/uniroma3/siw/GameHub/controller/GameController.java
+++ src/main/java/it/uniroma3/siw/GameHub/controller/GameController.java
@@ -54,5 +54,14 @@ public class GameController {
 			return null;
 		}
 	}
-	
+	@GetMapping("/compareGames/{id1}/{id2}")
+	public String compareGames(@PathVariable("id1") Long id1, @PathVariable("id2") Long id2, Model model) {
+		Iterable<Game> insiemeGiochi = this.gameService.findGiochiInComune(id1, id2);
+		if(insiemeGiochi==null) {
+			model.addAttribute("messaggioErrore", "nessun gioco in comune");
+		}else {
+			model.addAttribute("games", insiemeGiochi);
+		}
+		return "compareGames.html";
+	}
 }
--- src/main/java/it/uniroma3/siw/GameHub/service/GameService.java
+++ src/main/java/it/uniroma3/siw/GameHub/service/GameService.java
@@ -22,4 +22,8 @@ public class GameService {
 	public Game findById(Long id) {
 		return this.gameRepository.findById(id).orElse(null);
 	}
+
+	public Iterable<Game> findGiochiInComune(Long utente1Id, Long utente2Id) {
+		return this.gameRepository.findGiochiInComune(utente1Id, utente2Id);
+	}
 }
